800de3b0ffa3c4a674475ff10a3cb88883a8a7e3
Kledal <alex.kledal@gmail.com>
2015-05-07 14:21:06 +0200

    mend

--- machine.js
+++ machine.js
@@ -4,18 +4,20 @@ function Machine(uuid) {
   this.isConnected = false;
   this.client_id;
   this.info;
+  this.lastSeen = new Date();
 }
 
 Machine.prototype = {
   update: function(client_id, info) {
     this.client_id = client_id;
     this.info = info;
+    this.lastSeen = new Date();
   },
   connected: function() {
     this.isConnected = true;
   },
   printFile: function() {
-    
+
   }
 };
 
