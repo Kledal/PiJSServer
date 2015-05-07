ea142905779a27ff7d72ab88243e42869e7314b4
Kledal <alex.kledal@gmail.com>
2015-05-07 13:45:29 +0200

    mend

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ var server = http.createServer(function(request, response) {
     switch(uri) {
       case '/':
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(JSON.stringify(machines_connected));
+        response.write(JSON.stringify(machines));
         response.end();
       break;
 
