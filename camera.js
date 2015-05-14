59d287ad66aede8f0d5e10dcb41e6e8ea26dd953
Kledal <alex.kledal@gmail.com>
2015-05-14 10:03:00 +0200

    Update server to 5 sec

--- camera.js
+++ camera.js
@@ -2,7 +2,8 @@ function Camera(client_id) {
   this.client_id = client_id;
   this.uuid = null;
   this.lastFrame;
-  this.next_update = new Date().getTime() + 2;
+  this.requestTime = 5;
+  this.next_update = new Date().getTime() + this.requestTime;
 }
 
 Camera.prototype = {
@@ -12,7 +13,7 @@ Camera.prototype = {
   request_frame: function(connections) {
     if (new Date().getTime() > this.next_update) {
       connections[this.client_id].sendUTF( JSON.stringify([ ["ready_for_next_frame", { data: {} }] ]) );
-      this.next_update = new Date().getTime() + 2;
+      this.next_update = new Date().getTime() + this.requestTime;
     }
   }
 };
