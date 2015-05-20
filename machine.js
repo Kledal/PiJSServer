c5df893f03de4e582e41b744ce39d3f575adfc73
Kledal <alex.kledal@gmail.com>
2015-05-20 14:11:04 +0200

    Debug messages

--- machine.js
+++ machine.js
@@ -24,6 +24,7 @@ Machine.prototype = {
     clients[this.client_id].sendUTF( output );
   },
   start_print: function(clients, job_id, file_path) {
+    console.log("Starting print: " + job_id + ", file_path: " + file_path);
     var output = JSON.stringify([ ["run_job",
       {
         data: {
